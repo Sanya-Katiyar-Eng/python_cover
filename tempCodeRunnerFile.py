f9b33472693a9f28fcfdbd669f145adc690ed211
Sanya Katiyar <sanyakatiyar01@gmail.com>
2026-06-02 11:44:06 +0530

    update phy

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-conn=psycopg.connect(
-            host="localhost",
-            port=5432,
-            dbname="student_data_2024",
-            user="postgres",
-            password=1234
-            )
